b8e2e7a032b5d700aa37fe1368358de9ba978924
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-07-05 08:55:10 +0000

    backend code

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
