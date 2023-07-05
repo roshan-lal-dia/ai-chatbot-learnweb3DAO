75a26b1a525d56f15bb711f492b92905d5fea0b9
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-07-05 07:40:32 +0000

    added more code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,24 @@ import { useState } from "react";
 
 export default function Home() {
 
+  const [theInput, setTheInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: "Yo, this is ChatterBot! How can I help you today?",
+    },
+  ]);
+
+const Submit = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      callGetResponse();
+    }
+  };
+
   return (<div>Hello there DIA</div>)
   
   }
+
+  
